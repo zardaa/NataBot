f2de12f643a110ef3084c5773da7ae23a8c94a0c
zardaa <75716772+zardaa@users.noreply.github.com>
2020-12-23 20:01:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ const {
     wikien,
     covidcountry,
     infoanime,
+    loli,
     gay,
     ytmp3,
     ssweb,
@@ -888,35 +889,15 @@ case 'filmanime':
                .catch(err => {
                    console.log(err)
                })
-		   
-
-		   
-   	   
-		   
-		   
-   if (text.includes(".loli"))
-   {
-    var items = ["anime loli","anime loli sange","anime loli fackgirll","anime loli i love you"];
-    var nime = items[Math.floor(Math.random() * items.length)];
-    var url = "https://api.fdci.se/rep.php?gambar=" + nime;
-    
-    axios.get(url)
-      .then((result) => {
-        var n = JSON.parse(JSON.stringify(result.data));
-        var nimek =  n[Math.floor(Math.random() * n.length)];
-        imageToBase64(nimek) 
-        .then(
-            (response) => {
-	var buf = Buffer.from(response, 'base64'); 
-              conn.sendMessage(
-            id,
-              buf,MessageType.image)
-       
-            }
-        )
-        .catch(
-            (error) => {
-                console.log(error);
+		 break
+      case 'loli':
+           loli()
+               .then(buffer => {
+                   client.sendMessage(id, '[❗] WAIT', MessageType.text)
+                   client.sendMessage(id, buffer, MessageType.image)
+               })
+               .catch(err => {
+                   console.log(err)
                })
            break     
            case 'ssweb':
